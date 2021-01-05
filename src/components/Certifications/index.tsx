e0660c0ad0cdbc460ad1a5f2b04009c9843fb825
Arun Murugan <arunmurugan200@gmail.com>
2021-01-05 11:13:20 +0530

    Added animation for Certifications sections

--- src/components/Certifications/index.tsx
+++ src/components/Certifications/index.tsx
@@ -6,9 +6,11 @@ import styles from "./styles.module.scss";
 import externalLinkIcon from "../../images/link.svg";
 import { myCertifications } from "./certifications.data";
 
-interface Props {}
+interface Props {
+  disableAnimation?: boolean;
+}
 
-function Certifications({}: Props): ReactElement {
+function Certifications({ disableAnimation }: Props): ReactElement {
   const [limit, setLimit] = useState(5);
   return (
     <div
@@ -20,39 +22,46 @@ function Certifications({}: Props): ReactElement {
       <p className={commonStyles.secondaryText}>
         /* Learning never exhausts the mind */
       </p>
-      {myCertifications.slice(0, limit).map((o, i) => {
-        return (
-          <div key={i} className={styles.certificationContainer}>
-            <div>
-              {" "}
-              {o.name}
-              {" - "}
-              <span
-                className={commonStyles.secondaryText}
-                style={{ textTransform: "lowercase", margin: 0 }}
-              >
-                {o.organisation}
-              </span>
+      <div
+        data-aos={disableAnimation ? "" : "fade-up"}
+        data-aos-offset="200"
+        data-aos-easing="ease-out"
+        data-aos-duration="600"
+      >
+        {myCertifications.slice(0, limit).map((o, i) => {
+          return (
+            <div key={i} className={styles.certificationContainer}>
+              <div>
+                {" "}
+                {o.name}
+                {" - "}
+                <span
+                  className={commonStyles.secondaryText}
+                  style={{ textTransform: "lowercase", margin: 0 }}
+                >
+                  {o.organisation}
+                </span>
+              </div>
+              <Link target="_blank" rel="noopener" to={o.url}>
+                <img
+                  src={externalLinkIcon}
+                  alt={"external link to certificate"}
+                  style={{ height: "25px", width: "30px" }}
+                />
+              </Link>
             </div>
-            <Link target="_blank" rel="noopener" to={o.url}>
-              <img
-                src={externalLinkIcon}
-                alt={"external link to certificate"}
-                style={{ height: "25px", width: "30px" }}
-              />
-            </Link>
+          );
+        })}
+        {myCertifications.length > limit ? (
+          <div
+            className={commonStyles.underlinedLink}
+            onClick={() => setLimit(myCertifications.length)}
+            style={{ marginLeft: "10px", marginTop: "10px" }}
+          >
+            Show all
           </div>
-        );
-      })}
-      {myCertifications.length > limit ? (
-        <div
-          className={commonStyles.underlinedLink}
-          onClick={() => setLimit(myCertifications.length)}
-          style={{ marginLeft: "10px", marginTop: "10px" }}
-        >
-          Show all
-        </div>
-      ) : null}
+        ) : null}
+      </div>
     </div>
   );
 }
